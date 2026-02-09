311c647ef0d4f9053188f9a3cac38f299d6af8a1
Samuel Egwoyi <samuelegwoyi@gmail.com>
2026-02-09 02:26:54 +0100

    Change leaderboard data fetch interval to 30 seconds
    Updated the scheduled task to run every 30 seconds instead of every five minutes.

--- src/main/java/com/t2xafcon/leaderboard_service/service/LeaderboardServiceImpl.java
+++ src/main/java/com/t2xafcon/leaderboard_service/service/LeaderboardServiceImpl.java
@@ -102,10 +102,11 @@ public class LeaderboardServiceImpl implements LeaderboardService{
     }
 
     /**
-     * Runs every five minutes
+     * Runs every five minutes, changed to 30secs
      * Fetches leaderboard data from yellow dot
      * */
-    @Scheduled(fixedDelay = 300_000)
+    //@Scheduled(fixedDelay = 300_000)
+    @Scheduled(fixedDelay = 30_000)
     public void handleLeaderBoardDataFetch() {
         LeaderboardFetchDataDTO leaderboardData = yellowDotWebClient.get()
                 .uri(uri -> uri.path("/api/leaderboard").queryParam("pageNumber", 1).queryParam("pageSize", 100).build())
